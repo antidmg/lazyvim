b4a35dd3c837a8f51eab8107a240c6aaa5c53a18
antidmg <antidmg@proton.me>
2023-08-07 21:37:07 -0500

    initial config

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "python",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "rust",
+        "svelte",
+        "css",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ron", "rust", "toml" })
+      end
+    end,
+  },
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,33 @@
+local _, actions = pcall(require, "telescope.actions")
+return {
+  {
+    "telescope.nvim",
+    cmd = "Telescope",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+    opts = {
+      defaults = {
+        -- Cycle through choices with <C-j> and <C-k>
+        mappings = {
+          -- for input mode
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+          },
+          -- for normal mode
+          n = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+        },
+      },
+    },
+  },
+}
